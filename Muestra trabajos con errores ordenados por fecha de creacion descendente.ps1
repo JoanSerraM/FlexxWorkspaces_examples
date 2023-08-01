4d00589446717062c226a8ced01c40b2d0bf9c6c
Joan Serra <134929702+JoanSerraM@users.noreply.github.com>
2023-08-01 11:24:18 +0200

    Add files via upload

---
+++ Muestra trabajos con errores ordenados por fecha de creacion descendente.ps1
@@ -0,0 +1,25 @@
+﻿$api_url = 'https://fws-apim-93768.azure-api.net/api/'  # URL de la FWSAPI
+
+# Token en formato Basic
+$token = 'Basic NzkwODhkMzktNDE2ZC00ZTlmLTg4M2EtYTc4M2RhMzM4ODQwOm1oelNQRVQ0V3AxRmJ0Mip0OXVUckU1bkw='
+
+$headers = @{
+    'Content-Type' = 'application/json'
+    'Authorization' = $token
+}
+
+$url = $api_url + 'jobs?filter=startswith(Status,"Error")&orderby=CreationDate desc&apiversion=1'  # Endpoint de la API con filtro aplicado
+
+$response = $null
+$result = $null
+
+try {
+    $response = Invoke-RestMethod -Uri $url -Headers $headers -Method Get
+    $result = $response | ConvertTo-Json
+}
+catch {
+    throw $_.Exception.Message
+}
+
+# Mostrar la respuesta en pantalla
+$result | Format-Table
